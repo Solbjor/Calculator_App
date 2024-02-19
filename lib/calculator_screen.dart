5fa1940b4b89e328c97f451a8e3067941dd015a0
Solbjor <s0lbjorandr3w@gmail.com>
2024-02-19 00:19:55 -0800

    Complete
    Finished main calculating functions and organization.

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -85,7 +85,123 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   }
 
   //#############################
-  void onBtnTap(String value) {}
+  void onBtnTap(String value) {
+    if (value == Btn.del) {
+      delete();
+      return;
+    }
+
+    if (value == Btn.clr) {
+      clearAll();
+      return;
+    }
+
+    if (value == Btn.per) {
+      convertToPercentage();
+      return;
+    }
+
+    if (value == Btn.calculate) {
+      calculate();
+      return;
+    }
+
+    appendValue(value);
+  }
+
+  //#############################
+  //calculates the result
+  void calculate() {
+    if (number1.isEmpty) {
+      return;
+    }
+    if (operand.isEmpty) {
+      return;
+    }
+    if (number2.isEmpty) {
+      return;
+    }
+
+    final double num1 = double.parse(number1);
+    final double num2 = double.parse(number2);
+
+    var result = 0.0;
+    switch (operand) {
+      case Btn.add:
+        result = num1 + num2;
+        break;
+      case Btn.subtract:
+        result = num1 - num2;
+        break;
+      case Btn.multiply:
+        result = num1 * num2;
+        break;
+      case Btn.divide:
+        result = num1 / num2;
+        break;
+      default:
+    }
+
+    setState(() {
+      number1 = "$result";
+
+      if (number1.endsWith(".0")) {
+        number1 = number1.substring(0, number1.length - 2);
+      }
+
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  //#############################
+  //converts output to percentage
+
+  void convertToPercentage() {
+    if (number1.isNotEmpty && operand.isNotEmpty && number2.isNotEmpty) {
+      //calculate before conversion
+      calculate();
+      //final res = number1 operand number2;
+      //number1 = res;
+    }
+
+    if (operand.isNotEmpty) {
+      //cannot be converted
+      return;
+    }
+
+    final number = double.parse(number1);
+    setState(() {
+      number1 = "${(number / 100)}";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  //#############################
+  //clears all output
+  void clearAll() {
+    setState(() {
+      number1 = "";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  //#############################
+  //delete one value
+  void delete() {
+    if (number2.isNotEmpty) {
+      //ex: 12323 looks at last number => 1232
+      number2 = number2.substring(0, number2.length - 1);
+    } else if (operand.isNotEmpty) {
+      operand = "";
+    } else if (number1.isNotEmpty) {
+      number1 = number1.substring(0, number1.length - 1);
+    }
+
+    setState(() {});
+  }
 
   //#############################
   //appends value to the end
@@ -96,7 +212,8 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     if (value != Btn.dot && int.tryParse(value) == null) {
       //if is operand and not dot
       if (operand.isNotEmpty && number2.isNotEmpty) {
-        // TODO calculate the equation before assigning new values
+        //calculate the equation before assigning new values
+        calculate();
       }
       operand = value;
       //asign value to number1 variable
